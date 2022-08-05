96c5d78e6b1d24736a72f969395c269d8e062c34
Haobin Li <67214088+Haobin-Lee@users.noreply.github.com>
2022-08-05 15:05:47 +0800

    Delete leetcode/build directory

--- leetcode/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/C++ programs/新建文件夹
-# Build directory: D:/C++ programs/新建文件夹/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
